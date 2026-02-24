e30da35ad82557ff0491c90348beaacd76e0518f
gbrq8gkpth-arch <gbrq8gkpth@privaterelay.appleid.com>
2026-02-24 17:16:26 -0600

    Update run_model.py

--- run_model.py
+++ run_model.py
@@ -4,8 +4,7 @@ import ssl
 import pandas as pd
 import numpy as np
 from datetime import datetime
-from nba_api.stats.endpoints import scoreboardv2, leaguedashplayerstats, leaguedashteamstats
-
+from nba_api.stats.endpoints import scoreboardv2, leaguedashplayerstats, leaguedashteamstats, leaguedashptstats
 # ----------------------------
 # ENV VARIABLES
 # ----------------------------
