c8f06d7994c06886c57039ee5ed1c7521790e919
gbrq8gkpth-arch <gbrq8gkpth@privaterelay.appleid.com>
2026-02-24 20:37:52 -0600

    Update run_model.py

--- run_model.py
+++ run_model.py
@@ -23,7 +23,7 @@ def get_today_games():
         print("Calling scoreboard...")
         scoreboard = scoreboardv3.ScoreboardV3(game_date=today, timeout=10)
         games_df = scoreboard.get_data_frames()[0]
-        print("Scoreboard pulled.")
+        print(games_df.columns)
         return games_df
     except Exception as e:
         print("Scoreboard failed:", e)
